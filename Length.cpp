93b9c7bfaadab37a30cf3150c40f7504fb1fd5c9
convxz <m.kadzaev77@gmail.com>
2023-07-29 01:05:26 +0300

    learn system input-output

---
+++ Length.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int length; cin >> length;
+    cout << "length is: " << length << endl;
+    return 0;
+}
---
+++ readme.md
@@ -0,0 +1,1 @@
+this is my own way for learning c++ and other low-level languages of programming
